747e60908530744dfcfed5e625d00c8001b291c8
L1cHHH (Karateev Kirill) <kirill.karateev2016@gmail.com>
2025-09-30 00:46:26 +0300

    binaries was updated

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,8 @@
-fn main() {
 
+use mini_chat::client::connect_to_server;
+use mini_chat::config::ServerConfig;
+
+#[tokio::main]
+async fn main() {
+    connect_to_server(ServerConfig::new("192.168.147.1".to_string(), 8080)).await;
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,7 @@
-fn main() {
+use mini_chat::config::ServerConfig;
+use mini_chat::server::init_server;
 
+#[tokio::main]
+async fn main() {
+    init_server(ServerConfig::new("192.168.147.1".to_string(), 8080)).await
 }
